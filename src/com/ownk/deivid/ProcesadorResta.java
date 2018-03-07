e7e8511b944e08d258f34231771de34cb86866c4
Joan Torres <joan.torres@ownk.co>
2018-03-07 17:29:22 -0500

    li

---
+++ src/com/ownk/deivid/ProcesadorResta.java
@@ -0,0 +1,21 @@
+
+package com.ownk.deivid;
+import java.util.Scanner;
+
+public class ProcesadorResta {
+	public void resta() {
+
+
+		int restas;
+		System.out.println("Digite Numero uno:  ");
+		Scanner local = new Scanner(System.in);
+		int locals = local.nextInt();
+		System.out.println("Digite Numero dos: ");
+		Scanner segundi= new Scanner (System.in);
+		int segundis = segundi.nextInt();
+		restas= locals - segundis;
+		System.out.println("La resta entre "+locals + " y "+ segundis +"es: "+ restas);
+
+	}
+
+}
