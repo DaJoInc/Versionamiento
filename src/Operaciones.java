8403cf7ceecaa0208dfe03363513210bec28835e
Joan Torres <joan.torres@ownk.co>
2018-03-07 10:59:03 -0500

    suma

---
+++ src/Operaciones.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Operaciones {
+
+	public static void main(String[] args) {
+		int suma =0;
+		 System.out.print("Ingrese numero A::");
+			Scanner A = new Scanner(System.in);
+			 int a = A.nextInt();
+			 System.out.print("Ingrese numero B::");
+				Scanner B = new Scanner(System.in);
+				 int b = B.nextInt();
+				 suma = a+b;
+				 System.out.println("Total::"+suma);
+				 
+	}
+}
