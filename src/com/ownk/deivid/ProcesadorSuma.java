df24d4a72554b5b968a48a2d0af3fdad4188318a
Joan Torres <joan.torres@ownk.co>
2018-03-07 16:13:58 -0500

    desde resta

--- src/com/ownk/deivid/ProcesadorSuma.java
+++ src/com/ownk/deivid/ProcesadorSuma.java
@@ -5,7 +5,7 @@ public class ProcesadorSuma {
 public  void suma() {
 		
 		int suma =0;
-		 System.out.print("Ingrese numero A::");
+		 System.out.print("Ingrese numero A::A");
 			Scanner A = new Scanner(System.in);
 			 int a = A.nextInt();
 			 System.out.print("Ingrese numero B::");
