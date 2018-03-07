27714ca11120fb099b343c5a9aa126fc5eeca684
Joan Torres <joan.torres@ownk.co>
2018-03-07 12:02:13 -0500

    SE CREARRON DOS NUEVAS CLASES Y SE MODIFICO LA CLASE OPERACIONES

--- src/Operaciones.java
+++ src/Operaciones.java
@@ -3,15 +3,12 @@ import java.util.Scanner;
 public class Operaciones {
 
 	public static void main(String[] args) {
-		int suma =0;
-		 System.out.print("Ingrese numero A::");
-			Scanner A = new Scanner(System.in);
-			 int a = A.nextInt();
-			 System.out.print("Ingrese numero B::");
-				Scanner B = new Scanner(System.in);
-				 int b = B.nextInt();
-				 suma = a+b;
-				 System.out.println("Total::"+suma);
-				 
+		
+	 ProcesadorElegir procesadorElegir = new ProcesadorElegir();
+	 procesadorElegir.Elegir();
+	
+	
 	}
+	
+	
 }
