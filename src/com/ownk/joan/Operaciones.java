cf5f2921173b49194084e2198ddec18b6361bd43
Joan Torres <joan.torres@ownk.co>
2018-03-07 12:54:15 -0500

    arreglos

---
+++ src/com/ownk/joan/Operaciones.java
@@ -0,0 +1,16 @@
+package com.ownk.joan;
+import java.util.Scanner;
+
+public class Operaciones {
+
+	public static void main(String[] args) {
+		
+	 ProcesadorElegir procesadorElegir = new ProcesadorElegir();
+
+	 procesadorElegir.elegir();
+	
+	
+	}
+	
+	
+}
