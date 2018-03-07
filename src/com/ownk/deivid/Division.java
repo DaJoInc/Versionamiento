ec8706f468b7a1bec27c9cf0aff6355e2c18b9d8
Deivid Medina <Deivid.medina@ownk.co>
2018-03-07 14:48:04 -0500

    Fix

--- src/com/ownk/deivid/Division.java
+++ src/com/ownk/deivid/Division.java
@@ -3,21 +3,24 @@ package com.ownk.deivid;
 import java.util.Scanner;
 
 public class Division {
+	private Scanner local;
+	private Scanner segundi;
+
 	public void Divi() {
 
 
 		int divi;
 		int residuo;
 		System.out.println("Digite Numero uno: ");
-		Scanner local = new Scanner(System.in);
+		local = new Scanner(System.in);
 		int locals = local.nextInt();
 		System.out.println("Digite Numero dos: ");
-		Scanner segundi= new Scanner (System.in);
+		segundi = new Scanner (System.in);
 		int segundis = segundi.nextInt();
 		divi= locals / segundis;
-		residuo= locals%segundis;
-		System.out.println("La Multiplicación entre "+locals + " y "+ segundis +" es: "+ divi);
-		System.out.println("El residuo entre "+locals + " y "+ segundis +" es: "+ divi);
+		residuo= locals % segundis;
+		System.out.println("La division entre "+locals + " y "+ segundis +" es: "+ divi);
+		System.out.println("El residuo entre "+locals + " y "+ segundis +" es: "+ residuo);
 
 	}
 }
